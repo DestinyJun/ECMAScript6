53680459dbdc6f0fb8c7255b716a247c7a9632c2
DestinyJun <429771890@qq.com>
2020-02-08 23:51:40 +0800

    操作日志： （1）对象的扩展继续

--- 10-对象的扩展.js
+++ 10-对象的扩展.js
@@ -133,4 +133,68 @@
 
 /**
  * 属性的可枚举性和遍历
+ * 对象的每个属性都有一个描述对象（Descriptor），用来控制该属性的行为。
+ * Object.getOwnPropertyDescriptor方法可以获取该属性的描述对象。
+ */
+{
+  // 属性的可枚举基本案例
+  {
+    /**
+     * 描述对象的enumerable属性，称为“可枚举性”，如果该属性为false，就表示某些操作会忽略当前属性。
+     * 如如果为false，就会不被遍历到
+     * for...in循环：只遍历对象自身的和继承的可枚举的属性。
+     * Object.keys()：返回对象自身的所有可枚举的属性的键名。
+     * JSON.stringify()：只串行化对象自身的可枚举的属性。
+     * Object.assign()： 忽略enumerable为false的属性，只拷贝对象自身的可枚举的属性。
+     * 总的来说，操作中引入继承的属性会让问题复杂化，大多数时候，我们只关心对象自身的属性。所以，尽量不要用for...in循环，而用Object.keys()代替。
+     * @type {{name: number}}
+     */
+    let a = {name: 123,age: 18};
+    // console.log(Object.getOwnPropertyDescriptor(a,'name'));
+    // console.log(Object.keys(a));
+  }
+}
+
+/**
+ * 属性的遍历
+ * for...in循环遍历对象自身的和继承的可枚举属性（不含 Symbol 属性）。
+ * Object.keys返回一个数组，包括对象自身的（不含继承的）所有可枚举属性（不含 Symbol 属性）的键名。
+ * Object.getOwnPropertyNames返回一个数组，包含对象自身的所有属性（不含 Symbol 属性，但是包括不可枚举属性）的键名。
+ * Object.getOwnPropertySymbols返回一个数组，包含对象自身的所有 Symbol 属性的键名。
+ * Reflect.ownKeys返回一个数组，包含对象自身的所有键名，不管键名是 Symbol 或字符串，也不管是否可枚举。
+ * 以上的 5 种方法遍历对象的键名，都遵守同样的属性遍历的次序规则：
+ * -首先遍历所有数值键，按照数值升序排列。
+ * -其次遍历所有字符串键，按照加入时间升序排列。
+ * -最后遍历所有 Symbol 键，按照加入时间升序排列。
+ */
+
+/**
+ * super 关键字
+ * this关键字总是指向函数所在的当前对象，ES6 又新增了另一个类似的关键字super，指向当前对象的原型对象。
+ * 注意，super关键字表示原型对象时，只能用在对象的方法之中，用在其他地方都会报错。
+ * 目前，只有对象方法的简写法可以让 JavaScript 引擎确认，定义的是对象的方法。function字段以及箭头函数都会被认为是函数而不是对象的方法
+ */
+{
+  // 基本案例
+  {
+    const proto = {
+      foo: 'hello'
+    };
+    const obj = {
+      foo: 'world',
+      find() {
+        return super.foo;
+      },
+      show() {
+        return this.foo;
+      }
+    };
+    Object.setPrototypeOf(obj, proto);
+    // console.log(obj.find()); // "hello"
+    // console.log(obj.show()); // "world"
+  }
+}
+
+/**
+ * 对象的解构赋值
  */
